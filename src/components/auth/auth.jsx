76e51bbfaa795ccbf334df4f883074eb7ba3d3c0
Vladimir Khodakov <hodakovvladimir1990@gmail.com>
2021-09-22 17:35:00 +0300

    UseState for auth

--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import classes from './style/auth.module.css'
 
 function Auth() {
+  const [Login, setLogin] = useState('')
+  const [Pass, setPass] = useState('')
+
+  function login(e) {
+    alert([Login, Pass])
+    e.preventDefault()
+  }
+  
+  function registration(e) {
+    alert("registration")
+    e.preventDefault()
+  }
+
   return (
     <div className={classes.Container}>
         <form className={classes.authForm} action="alert(1)" method="post" autocomplete="off">
-          <input className={classes.formItem} type="text" id="name" name="user_name" placeholder=" Логин"/>
-          <input className={classes.formItem}  type="password" id="password" name="user_passwordc" placeholder=" Пароль"/>
+          <input 
+            className={classes.formItem} type="text" id="name" name="user_name" placeholder=" Логин" 
+            value={Login}
+            onChange={event => setLogin(event.target.value)}
+          />
+          <input className={classes.formItem}  type="password" id="password" name="user_passwordc" placeholder=" Пароль"
+          value={Pass}
+          onChange={event => setPass(event.target.value)}
+          />
           <button className={classes.formItem} type="submit" onClick={login}>Войти</button>
           <button className={classes.formItem} type="submit" onClick={registration}>Регистрация</button>
         </form>
@@ -15,13 +35,3 @@ function Auth() {
 }
 
 export default Auth
-
-function login(e) {
-  alert("login")
-  e.preventDefault()
-}
-
-function registration(e) {
-  alert("registration")
-  e.preventDefault()
-}
